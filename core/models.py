2570f558b511a09c0cb6428cbca96b84826466c6
Renato César <renatogarcia1@hotmail.com>
2020-05-31 21:57:32 -0300

    Adicionado funções de alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -17,5 +17,8 @@ class Evento(models.Model):
         return self.titulo
 
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%m/%Y - %H:%M Hrs')
+        return self.data_evento.strftime('%d/%m/%Y  %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
